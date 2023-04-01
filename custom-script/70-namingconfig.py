2efb1f8ed70e296dc3f359f66ac85b28367516c7
bizalu <chprok@gmail.com>
2023-04-01 23:30:34 +0200

    debug NamingConfig

--- custom-script/70-namingconfig.py
+++ custom-script/70-namingconfig.py
@@ -14,6 +14,57 @@ RADARR_DB = '/config/radarr.db'
 ###########################################################
 # DEFINE FUNCTION
 ###########################################################
+def set_namingconfig(database, enable):
+    if enable == "True":
+        naming = 1
+    else:
+        naming = 0
+    data = (0, 1, '{Movie Title} ({Release Year}) {Quality Full}', '{Movie Title} ({Release Year})', 0, naming)
+    query = "INSERT INTO NamingConfig (MultiEpisodeStyle,ReplaceIllegalCharacters,StandardMovieFormat," \
+            "MovieFolderFormat,ColonReplacementFormat,RenameMovies) VALUES(?, ?, ?, ?, ?, ?)"
+    connexion = sqlite3.connect(database)
+    db = connexion.cursor()
+
+    try:
+        db.execute(query, data)
+        connexion.commit()
+
+        db.close()
+        connexion.close()
+    except sqlite3.Error as er:
+        logging.error('SQLite error: %s' % (' '.join(er.args)))
+        return None
+    else:
+        return enable
+
+
+def get_namingconfig(database):
+    query = "SELECT RenameMovies FROM NamingConfig"
+
+    connexion = sqlite3.connect(database)
+    db = connexion.cursor()
+
+    try:
+        db.execute(query)
+        rows = db.fetchall()
+
+        db.close()
+        connexion.close()
+
+        if not rows:
+            raise ValueError
+    except sqlite3.Error as er:
+        logging.error('SQLite error: %s' % (' '.join(er.args)))
+        return None
+    except ValueError:
+        return ""
+    else:
+        if rows[0][0] == 1:
+            return "True"
+        else:
+            return "False"
+
+
 def update_namingconfig(database, enable):
     if enable == "True":
         query = "UPDATE NamingConfig SET RenameMovies = 1"
@@ -47,6 +98,16 @@ if __name__ == '__main__':
         sys.exit(0)
 
     logging.info("Set Naming Config to application with value %s ..." % RADARR_NAMING)
-    NAMING = update_namingconfig(RADARR_DB, RADARR_NAMING)
+    NAMING = get_namingconfig(RADARR_DB)
     if NAMING is None:
         sys.exit(1)
+
+    if NAMING == "":
+        NAMING = set_namingconfig(RADARR_DB, RADARR_NAMING)
+        if NAMING is None:
+            sys.exit(1)
+
+    if NAMING != RADARR_NAMING:
+        NAMING = update_namingconfig(RADARR_DB, RADARR_NAMING)
+        if NAMING is None:
+            sys.exit(1)
