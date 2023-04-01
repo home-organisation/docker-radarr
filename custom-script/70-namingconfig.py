88d2f48f06ca5f76a0364f862f59896cb4f181a5
bizalu <chprok@gmail.com>
2023-04-01 22:48:15 +0200

    add NamingConfig

---
+++ custom-script/70-namingconfig.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import sys
+import os
+import sqlite3
+import logging
+
+###########################################################
+# SET STATIC CONFIG
+###########################################################
+logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+RADARR_DB = '/config/radarr.db'
+
+
+###########################################################
+# DEFINE FUNCTION
+###########################################################
+def update_namingconfig(database, enable):
+    if enable == "True":
+        rename = 1
+    else:
+        rename = 0
+    data = (rename,)
+    query = "UPDATE NamingConfig SET RenameMovies = ?"
+
+    connexion = sqlite3.connect(database)
+    db = connexion.cursor()
+
+    try:
+        db.execute(query, data)
+        connexion.commit()
+
+        db.close()
+        connexion.close()
+    except sqlite3.Error as er:
+        logging.error('SQLite error: %s' % (' '.join(er.args)))
+        return None
+    else:
+        return rename
+
+
+###########################################################
+# INIT CONFIG
+###########################################################
+if __name__ == '__main__':
+    RADARR_NAMING = os.environ.get('RADARR_NAMING')
+    if RADARR_NAMING is None or RADARR_NAMING not in ["True", "False"]:
+        logging.warning("RADARR_NAMING <%s> is empty or has unaccepted value (True or False), "
+                        "nothing to do" % RADARR_NAMING)
+        sys.exit(0)
+
+    logging.info("Set Naming Config to application with value %s ..." % RADARR_NAMING)
+    NAMING = update_namingconfig(RADARR_DB, RADARR_NAMING)
+    if NAMING is None:
+        sys.exit(1)
